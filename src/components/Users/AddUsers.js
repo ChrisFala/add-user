74c14fcbb00d7b11d02128aea95a13f963c77944
ChrisFala <christopherfala42@gmail.co>
2022-11-03 15:54:02 -0700

    Add Users tracks field state & clears fields on submit

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,14 +1,38 @@
-const AddUser = props => {
+import { useState } from "react";
+
+const AddUser = (props) => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value)
+    }
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value)
+    }
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        setEnteredUsername('')
+        setEnteredAge('')
     }
-    
+
     return (
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text"/>
+            <input
+                id="username"
+                type="text"
+                value={enteredUsername}
+                onChange={usernameChangeHandler}
+            />
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number"/>
+            <input
+                id="age"
+                type="number"
+                value={enteredAge}
+                onChange={ageChangeHandler}
+            />
             <button type="submit">Add User</button>
         </form>
     );
