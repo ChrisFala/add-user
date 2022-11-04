79c66d264fef2a989577d2297c3713ef315073e3
ChrisFala <christopherfala42@gmail.co>
2022-11-04 15:57:25 -0700

    Added CSS styling to form. Working on valid input check.

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from './AddUsers.module.css'
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -6,6 +7,7 @@ const AddUser = (props) => {
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value)
+        console.log(event.target.value)
     }
     const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value)
@@ -19,21 +21,21 @@ const AddUser = (props) => {
 
     return (
         <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
+            <label className={styles.label} htmlFor="username">Username</label>
             <input
                 id="username"
                 type="text"
                 value={enteredUsername}
                 onChange={usernameChangeHandler}
             />
-            <label htmlFor="age">Age (Years)</label>
+            <label className={styles.label} htmlFor="age">Age (Years)</label>
             <input
                 id="age"
                 type="number"
                 value={enteredAge}
                 onChange={ageChangeHandler}
             />
-            <button type="submit">Add User</button>
+            <button className={styles.button} type="submit">Add User</button>
         </form>
     );
 };
