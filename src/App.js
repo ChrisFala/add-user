7691b2b5bc19f9ae4c1e63cf30509b58024aaeb9
ChrisFala <christopherfala42@gmail.com>
2022-12-11 09:16:11 -0700

    Fixed User Data Tracking Error
    Wrong component was being used in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import NewUser from './components/Users/NewUser'
 
 
 function App() {
   return (
     <div>
-      <AddUser/>
+      <NewUser/>
     </div>
   );
 }
